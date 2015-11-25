4af16586366aaaf579a6f98a6325868abd106b61
RichardzWang <rwang4@stuy.edu>
2015-11-24 20:36:54 -0500

    PHASE 4 COMPLETE

--- Rational.java
+++ Rational.java
@@ -42,81 +42,90 @@ public class Rational {
         	this.numerator *= other.denominator;
         	this.denominator *= other.numerator;
     	} else {
-    		System.out.println("Cannot divide by 0.")
+	    System.out.println("Cannot divide by 0.");
     	}
     }
     
     public static void main(String[] args) {
         Rational r = new Rational(1, 1);
         Rational s = new Rational(7, 20);
-        s.compareTo(r);
-        System.out.println(s);
+	Rational t = new Rational(14, 40);
+	s.compareTo(r);
+        System.out.println(s.compareTo(r));
+	System.out.println(s.equals(t));
+     }
+
+  /*=====================PHASE 2=====================*/
+
+     public void add(Rational other){
+	 this.numerator *= other.denominator;
+	 this.numerator += (other.numerator * this.denominator);
+	 this.denominator *= other.denominator;
+     }
+
+     public void subtract(Rational other){
+	 this.numerator *= other.denominator;
+	 this.numerator -= (other.numerator * this.denominator);
+	 this.denominator *= other.denominator;
+     }
+
+     public int gcdEW(){
+	 int gcd = 0;
+	 int a = this.numerator;
+	 int b = this.denominator;
+	     if (a == 0){
+		 return b;
+	     }
+	 while (a != 0 || b != 0){
+	     if (a > b) {
+		 if (a % b == 0){
+		     gcd = b;
+		 }
+		 a = a % b;
+	     }
+	     else {
+		 if ( b % a == 0){
+		     gcd = a;
+		 }
+		 b = b % a;
+	     }
+	 }
+	 return gcd;
+     }
+
+     public void reduce(){
+	 int gcd = gcdER(this.numerator, this.denominator);
+	 this.numerator /= gcd;
+	 this.denominator /= gcd;
+     }
+
+     /*=====================PHASE 3=====================*/
+     public static int gcdER (int a, int b){
+	 if ((a == b) || (b == 0)){
+	     return a;} //returns the GCD.
+	 else if (a < b){
+	     return gcdER (b,a);} //If b is greater than a, the function will be run again with both values swapped.
+	 else{
+	     return gcdER (b , (a-b));
+	 }
+     }
+
+     public int compareTo (Rational other){
+	 if (this.floatValue() > other.floatValue()) {
+	     return 1;
+	 } else if (this.floatValue() < other.floatValue()) {
+	     return -1;
+	 } else {
+	     return 0;
+	 }
     }
 
- /*=====================PHASE 2=====================*/
-    
-    public void add(Rational other){
-        int thisOriginal = this.denominator;
-        if (this.denominator != other.denominator){
-            this.numerator *= other.denominator;
-            this.denominator *= other.denominator;
-            other.numerator *= thisOriginal;
-        }
-        this.numerator += other.numerator;
-    }
-    
-    public void subtract(Rational other){
-        int thisOriginal = this.denominator;
-        if (this.denominator != other.denominator){
-            this.numerator *= other.denominator;
-            this.denominator *= other.denominator;
-           }
-        this.numerator -= other.numerator * thisOriginal;
-    }
-    
-    public int gcdEW(){
-        int gcd = 0;
-        int a = this.numerator;
-        int b = this.denominator;
-	    if (a == 0){
-	        return b;
-	    }
-        while (a != 0 || b != 0){
-            if (a > b) {
-                if (a % b == 0){
-		    gcd = b;
-		}
-                a = a % b;
-            }
-            else {
-                if ( b % a == 0){
-		    gcd = a;
-		}
-                b = b % a;
-            }
-        }
-        return gcd;
-    }
-    
-    public void reduce(){
-        int gcd = gcdER(this.numerator, this.denominator);
-        this.numerator /= gcd;
-        this.denominator /= gcd;
-    }
-    
-    /*=====================PHASE 3=====================*/
-    public static int gcdER (int a, int b){
-        if ((a == b) || (b == 0)){
-            return a;} //returns the GCD.
-        else if (a < b){
-            return gcdER (b,a);} //If b is greater than a, the function will be run again with both values swapped.
-        else{
-            return gcdER (b , (a-b));
-        }
-    }
-    
-    public float compareTo (Rational other){
-        other.subtract(this);//figuring out which one is larger by taking difference of the two
-        return this.numerator / this.denominator;//returning "Rational this" which fits the function's requirements 
+    /*=====================PHASE 4=====================*/
+    public boolean equals(Object other){
+	boolean retboo = this == other;
+	if (!retboo) {
+	    retboo = (other instanceof Rational && (((Rational)this).compareTo((Rational)other))==0);
+	}
+	return retboo;
     }
 }
